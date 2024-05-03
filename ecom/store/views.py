8bba851413e8a0ead4b9bc75d411ffbc3057a9af
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-05-03 15:57:59 +0300

    Adding log in logout view functions

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -7,3 +7,11 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def login_user(request):
+    return render(request, 'login.html', {})
+
+def logout_user(request):
+    pass
+    
