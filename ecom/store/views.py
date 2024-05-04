dc118c27e6a342f1a150b16180584ec2f4d3c5de
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-05-04 19:30:29 +0300

    Adding the register_user initial view

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -30,5 +30,10 @@ def logout_user(request):
     logout(request)
     messages.success(request,("You have logged out... See you again !"))
     return redirect('home')
+
+def register_user(request):
+        return render(request, 'register.html', {})
+    
+    
     
     
