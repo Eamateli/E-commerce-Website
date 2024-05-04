3fe0061fd95dc78234782fa7cccaca0ce8b08204
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-05-04 19:51:56 +0300

    Adding logic to the register_user view

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User 
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 def home(request):
     products = Product.objects.all()
@@ -32,6 +36,23 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request,("You have registered successfully. Welcome!"))
+            return redirect('home')
+        else:
+            messages.success(request,("Whooops! There was a problem registering. Please try again..."))
+            return redirect('register')
+            
+            
+    else:
         return render(request, 'register.html', {})
     
     
