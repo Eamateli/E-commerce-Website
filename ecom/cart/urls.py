ba5b0607ebbbf7a56992d4d8d96299b6fae3f647
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-05-07 13:57:00 +0300

    Adding views for add, delete, update

--- ecom/cart/urls.py
+++ ecom/cart/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     
+    path('', views.cart_summmary, name='cart_summary'),
+    path('add/', views.cart_add, name='cart_add'),
+    path('delete/', views.cart_delete, name='cart_delete'),
+    path('update/', views.cart_update, name='cart_update'),
     
+
 ]
