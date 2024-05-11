19c0325d652762064083077c251faf3f919ea3ba
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-05-11 19:00:29 +0300

    Adding logic for the total and for the sale price

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -35,13 +35,16 @@ class Cart():
         #Get product IDs
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=product_ids)
-        quantities = self.cart()
+        quantities = self.cart
         total = 0
         for key, value in quantities.items():
             key = int(key)
             for product in products:
                 if product.id == key :
-                    total = total + (product.price * value)
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else:
+                        total = total + (product.price * value)            
         return total
                     
         
