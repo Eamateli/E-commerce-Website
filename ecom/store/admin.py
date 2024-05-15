28bf6e3968b77ef9f0ba32c48c1d42a676db2bdd
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-05-15 23:22:34 +0300

    Extending the user model on the admin panel

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -11,3 +11,21 @@ admin.site.register(Order)
 admin.site.register(Profile)
 
 
+#Mix profile info and user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    
+    
+# Extend the user Model
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username", "first_name", "last_name", "email"]
+    inlines = [ProfileInline]
+    
+# Unregister old way
+admin.site.unregister(User)
+
+# Re-Register the new way 
+admin.site.register(User, UserAdmin)
+
