8cbabc24cb58b97ea61a778a52c842fa4330b40f
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-05-15 23:15:14 +0300

    Adding the profile to the admin.py

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Category, Customer, Product, Order
+from django.contrib.auth.models import User
+from .models import Profile
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Profile)
+
 
