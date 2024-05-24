88523c6c833bb27b0d942b59f6cad42d2d05cbbe
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-05-24 23:54:36 +0300

    Adding the form view

--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from cart.cart import Cart
 from payment.forms import ShippingForm
+from .models import ShippingAddress
 
 
 def payment_success(request):
@@ -13,21 +14,15 @@ def checkout(request):
     quantities = cart.get_quants
     totals = cart.cart_total()
     if request.user.is_authenticated:
-     
-        # User chekcout
-        
-        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)		
-         
-        render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities": quantities, "totals":totals}) 
-     
-     
-     
+		# Checkout as logged in user
+		# Shipping User
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+		# Shipping Form
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form })
     else:
-     
-        # Guest chekcout
-        
-        
-        shipping_form = ShippingForm(request.POST or None)		
-        
-        return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities": quantities, "totals":totals}) 
+		# Checkout as guest
+        shipping_form = ShippingForm(request.POST or None)
+        return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+
     
