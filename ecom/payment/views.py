db826c31128d8fb914a851a5c182862f1b010d3b
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-05-26 20:35:06 +0300

    Adding a view

--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -4,6 +4,17 @@ from payment.forms import ShippingForm
 from .models import ShippingAddress
 
 
+def billing_info(request):
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+    shipping_form = request.POST
+    return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+    
+    
+
+
 def payment_success(request):
     return render(request, "payment/payment_success.html", {})
 
