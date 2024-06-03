5007635e98a21d6b59475eb481275e8660df93bd
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-06-03 14:27:33 +0300

    Creating the process_order view. Lots of work

--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -45,7 +45,7 @@ class Order(models.Model):
     full_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     shipping_address = models.TextField(max_length=15000)
-    amount_pay = models.DecimalField(max_digits=10, decimal_places=2)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
