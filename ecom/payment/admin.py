80cd6e90b0da66023c21e411f21860980c250729
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-06-05 21:44:19 +0300

    Creating an Inline order item

--- ecom/payment/admin.py
+++ ecom/payment/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import ShippingAddress,Order, OrderItem
+from django.contrib.auth.models import User
 
 admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+class OrderItemInline(admin.StackedInline):              # Create an OrderItem Inline
+    model = OrderItem
+    
+    
+class OrderAdmin(admin.ModelAdmin):                      #Extend our Order Model
+    model = Order
+    inlines = [OrderItemInline]
