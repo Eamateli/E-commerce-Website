177edb6dac57ccd571da7a85dc6951b4ab4d9c5c
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-06-06 15:48:02 +0300

    Adding a new field

--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -48,6 +48,7 @@ class Order(models.Model):
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
     shipped = models.BooleanField(default=False)
+    date_shipped = models.DateTimeField(blank=True, null=True)
     
     def __str__(self):
         return f"Order - {str(self.id)}"
