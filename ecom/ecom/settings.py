4d4430480a906db0573e78ec562b36ace8f90447
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-06-12 22:51:03 +0300

    Setting up a postgresDB on railway

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -80,8 +80,15 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'wzvZKqGKHumBhzejMQHXSVyZjHkiveOH',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '29533',
+        
     }
 }
 
