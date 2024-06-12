18d84c07ba65400a17a6651a8950ffec78291342
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-06-12 23:07:53 +0300

    Making migrations

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+#Load environamental variables 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'wzvZKqGKHumBhzejMQHXSVyZjHkiveOH',
+        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
         'HOST': 'monorail.proxy.rlwy.net',
         'PORT': '29533',
         
