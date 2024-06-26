d37065f87832df2859c1b0c50144a0fd69d31bfe
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-06-26 22:35:14 +0300

    Fixing some stuff

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-5sifm367+i+*tdopzz+)0s&h8lqbc7w3ow=!bgy-9%##2j9=$m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['e-commerce-website-production-5777.up.railway.app', 'https://e-commerce-website-production-5777.up.railway.app']
+ALLOWED_HOSTS = ['e-commerce-website-production-5777.up.railway.app', 'https://e-commerce-website-production-5777.up.railway.app','http://e-commerce-website-production-5777.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://e-commerce-website-production-b123.up.railway.app']
 
 
