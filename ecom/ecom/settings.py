805dd55ca41cbb6a296b07a71104185604412f24
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-07-23 14:28:37 +0300

    installing django-paypal and adding paypal settings

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'cart',
     'payment',
     'whitenoise.runserver_nostatic',
+    'paypal.standard.ipn',
 ]
 
 MIDDLEWARE = [
@@ -154,3 +155,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Paypal settings
+
+PAYPAL_TEST = True
+PAYPAL_RECEIVERVER_EMAIL = 'business@testdjangoapp.com'
