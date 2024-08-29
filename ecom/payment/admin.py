042c0bf1d9171ef4e3ffc95408743ecb8178abb1
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-08-29 19:42:38 +0300

    Adding the invoice and paid on the admin dashboard

--- ecom/payment/admin.py
+++ ecom/payment/admin.py
@@ -14,7 +14,7 @@ class OrderItemInline(admin.StackedInline):              # Create an OrderItem I
 class OrderAdmin(admin.ModelAdmin):                      #Extend our Order Model
     model = Order
     readonly_fields = ["date_ordered"]
-    fields = ["user", "full_name", "email", "shipping_address", "amount_paid", "date_ordered", "shipped","date_shipped"]
+    fields = ["user", "full_name", "email", "shipping_address", "amount_paid", "date_ordered", "shipped","date_shipped", "invoice","paid"]
     inlines = [OrderItemInline]
     
     
